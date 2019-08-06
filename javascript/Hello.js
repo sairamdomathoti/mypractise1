50e4da5b4058b9f062b161074242bdb9a9baaebb
sairamdomathoti <sairam.domathoti@gmail.com>
2019-08-06 10:19:39 +0530

    feat: added javasscript examples

---
+++ javascript/Hello.js
@@ -0,0 +1,4 @@
+console.log("welcome to Hello Java good");
+    alert("welcome to Hello Java good alert");
+    // alert("comments");
+    
---
+++ javascript/Variables.js
@@ -0,0 +1,39 @@
+//integer
+var a = 10;
+// printing value
+console.log("a value :" + a);
+
+// printing type of value
+console.log("Type of a value : " + typeof(a));
+
+// string
+var b="Sairam";
+console.log("b value :" + b);
+console.log("Type of b value : " + typeof(b));
+
+// boolean
+var bool=true;
+console.log("bool value :" + bool);
+console.log("Type of bool value : " + typeof(bool));
+
+
+// array
+var arr=[1,2,3,4,5];
+console.log("arr value :" + arr);
+console.log("Type of arr value : " + arr.length);
+
+// object
+var obj={"id" :"101","name": "Sairam"};
+console.log("obj value :" + obj);
+console.log("obj value :" + JSON.stringify(obj));
+console.log("obj value :"  + typeof(obj));
+console.log("col of id value : " + obj.id);
+console.log("col of name value : " + obj.name);
+
+// Parse
+var obj1={"id" :"101","name": "Sairam"};
+console.log(obj1);
+console.log("obj1 value :" + JSON.stringify(obj1));
+console.log("obj1 value :"  + typeof(obj1));
+console.log("col of id value : " + obj1.id);
+console.log("col of name value : " + obj1.name);
